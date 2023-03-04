429d131975a87f41510ca6e71e48be577b6c6863
vatsala sonal <vsonal1985@gmail.com>
2023-03-04 12:01:45 -0800

    removed duplicated tests

--- app/src/main/java/com/issapp/utils/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.issapp.utils
-
-object Constants {
-    const val BASE_URL = "http://api.open-notify.org"
-    const val ISS_LOCATION = "/iss-now.json"
-    const val ISS_ASTRONAUTS = "/astros.json"
-}
